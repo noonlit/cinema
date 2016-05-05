d6e538f6becdd905638cafb68e917043495f462e
Andra B <julietfeynman@gmail.com>
2016-05-05 16:56:20 +0300

    Hopefully fixed autoload issue part 2

--- config/prod.php
+++ config/prod.php
@@ -2,5 +2,9 @@
 
 // configure your app for the production environment
 
+$app['config'] = require 'config.php';
+
 $app['twig.path'] = array(__DIR__.'/../templates');
 $app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+
+// set up repo factory and pdo-related things
