7cd4ef6d09bc83ded4c7681622377676a56577b1
Andra B <julietfeynman@gmail.com>
2016-05-06 10:06:37 +0300

    [feature/repo-create] Fixed a method call and some formatting

--- src/Repository/AbstractRepository.php
+++ src/Repository/AbstractRepository.php
@@ -6,12 +6,14 @@ use Doctrine\DBAL\Connection;
 
 abstract class AbstractRepository // not sure it will turn out to be v abstract
 {
-	protected $dbConnection;
-	protected $tableName;
-	
-	public function __construct(Connection $dbConnection, $tableName)
+
+    protected $dbConnection;
+    protected $tableName;
+
+    public function __construct(Connection $dbConnection, $tableName)
     {
         $this->dbConnection = $dbConnection;
-		$this->tableName = $tableName;
+        $this->tableName = $tableName;
     }
+
 }
--- src/Repository/UserRepository.php
+++ src/Repository/UserRepository.php
@@ -6,13 +6,11 @@ use Doctrine\DBAL\Connection;
 
 class UserRepository extends AbstractRepository
 {
-	public function insert() // this will take a parameter - $user - which will be an entity, and we'll get its properties and store them (?)
-	{
-		 $this->dbConnection->insert($this->tableName, array('username' => 'Miauuu',
-			 'password' => 'doubleMiau',
-			 'email' => 'email@miau.com',
-			 'active' => true,
-			 'role' => 2));
-		// with the entity as parameter, this would look smth like $this->dbConnection->insert($this-tableName, array('name' => $user->getName()));
-	}
+
+    public function insert() // this will take a parameter - $user - which will be an entity, and we'll get its properties and store them (?)
+    {
+        // $this->dbConnection->insert($this->tableName, array('email' => 'm@m.com'));
+        // with the entity as parameter, this would look smth like $this->dbConnection->insert($this-tableName, array('name' => $user->getName()));
+    }
+
 }
