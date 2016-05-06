bdbd95613dd49800ef3df5fd1399966de82805dd
deliaed <deliatps@outlook.com>
2016-05-06 20:18:24 +0300

    finished entity & added validator

--- src/Repository/MovieRepository.php
+++ src/Repository/MovieRepository.php
@@ -5,4 +5,17 @@ namespace Repository;
 class MovieRepository extends AbstractRepository
 {
 
+    /**
+     * creates \Entity\Movie object from properties array
+     * 
+     * @param array $properties
+     * @return \Entity\Movie
+     */
+    protected function loadEntityFromArray(array $properties) {
+        
+        //validate dis shit
+        
+        return new \Entity\Movie($properties);
+    }
+    
 }
