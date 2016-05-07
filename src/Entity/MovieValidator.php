ddfd546ee80f642847181526da08ca985fed187a
Andra B <julietfeynman@gmail.com>
2016-05-07 16:25:30 +0300

    [feature/entity-and-repo-integration] Integrated movie repo + entity

--- src/Entity/MovieValidator.php
+++ src/Entity/MovieValidator.php
@@ -3,7 +3,7 @@ namespace Entity;
 
 class MovieValidator {
     
-    
+    // can't the movie entity have something like a method isValid() that returns true if all the properties are set? or ... ? not sure what goes in here/there
     
 }
 
