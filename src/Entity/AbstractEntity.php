68ea803ef8eaabf38c0c1264388df398c22f14ea
Andra B <julietfeynman@gmail.com>
2016-05-07 16:17:44 +0300

    [feature/abstract-repo] Added a construct to abstract entity

--- src/Entity/AbstractEntity.php
+++ src/Entity/AbstractEntity.php
@@ -6,6 +6,15 @@ abstract class AbstractEntity
 {
     protected $id;
     
+    public function __construct($properties)
+    {
+        foreach ($properties as $key => $value) {
+            if (property_exists($this, $key)) {
+                $this->$key = $properties[$key];
+            }
+        }
+    }
+    
     public function getId()
     {
         return $this->id;
