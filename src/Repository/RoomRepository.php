e5a26b95c0b13b5501800352b88c4d3c005eed34
Georgiana Tobosi <t_georgiana@ymail.com>
2016-05-09 10:19:22 +0300

    rooms

--- src/Repository/RoomRepository.php
+++ src/Repository/RoomRepository.php
@@ -2,7 +2,27 @@
 
 namespace Repository;
 
+use Entity\Room;
+
+
 class RoomRepository extends AbstractRepository
 {
-    
+   
+    /**
+     * make Room entity from array
+     * @param array $properties
+     * @return Room
+     */
+    private function loadEntityFromArray(array $properties)
+    {
+        return new \Entity\Room($properties);
+    }
+    /**
+     * make an array from Room entity
+     * @param Room $entity
+     */
+    private function loadArrayFromEntity(\Entity\Room $entity) {
+        
+        parent::loadArrayFromEntity($entity);
+    }
 }
