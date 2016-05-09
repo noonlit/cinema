dc6ba01eb7ca6cef093acfdbfdb45cad66b3aad2
AndreiBodea0 <andreicalinb@gmail.com>
2016-05-09 12:24:47 +0300

    [develop] changed db structure and tested bookings

--- config/prod.php
+++ config/prod.php
@@ -2,28 +2,28 @@
 
 // configure your app for the production environment
 
-$app['twig.path'] = array(__DIR__.'/../templates');
-$app['twig.options'] = array('cache' => __DIR__.'/../var/cache/twig');
+$app['twig.path'] = array(__DIR__ . '/../templates');
+$app['twig.options'] = array('cache' => __DIR__ . '/../var/cache/twig');
 
 // set up repo factory and pdo-related things
 
 /* all things database-related */
-$app['config'] = require __DIR__.'/../config/config.php';
+$app['config'] = require __DIR__ . '/../config/config.php';
 
 
 $app->register(new Silex\Provider\DoctrineServiceProvider(), array(
     'db.options' => array(
-		'dbname' => 'cinemaDatabase',
-		'user' => $app['config']['database']['user'],
-		'password' => $app['config']['database']['password'],
-		'host' => 'localhost',
-		'port' => 3306,
-		'charset' => 'utf8',
-		'driver' => 'pdo_mysql',
+        'dbname' => 'cinemadatabase',
+        'user' => $app['config']['database']['user'],
+        'password' => $app['config']['database']['password'],
+        'host' => 'localhost',
+        'port' => 3306,
+        'charset' => 'utf8',
+        'driver' => 'pdo_mysql',
     ),
 ));
 
-/* 
+/*
  * Repositories
  */
 
@@ -36,22 +36,26 @@ $app['booking_repository'] = Repository\RepositoryFactory::getRepository('bookin
 
 
 /* Projected income query test
-$firstDate = new \DateTime();
-$firstDate->setDate(2016, 5, 5);
-$secondDate = new \DateTime();
-$secondDate->setDate(2016, 5, 6);
-$projectedIncome = $app['schedule_repository']->getProjectedIncomeBetween($firstDate, $secondDate);
-echo $projectedIncome;*/
+  $firstDate = new \DateTime();
+  $firstDate->setDate(2016, 5, 10);
+  $secondDate = new \DateTime();
+  $secondDate->setDate(2016, 5, 10);
+  $projectedIncome = $app['schedule_repository']->getProjectedIncomeBetween($firstDate, $secondDate);
+  echo $projectedIncome; */
 
 /* Second projected income query test 
 $firstDate = new \DateTime();
-$firstDate->setDate(2016, 5, 5);
+$firstDate->setDate(2016, 5, 7);
 $secondDate = new \DateTime();
-$secondDate->setDate(2016, 5, 6);
-$projectedIncome = $app['schedule_repository']->getProjectedIncomeForMovieBetween($firstDate, $secondDate, "Romeo and Julieta");
- */
+$secondDate->setDate(2016, 5, 10);
+$projectedIncome = $app['schedule_repository']->getProjectedIncomeForMovieBetween($firstDate, $secondDate, "Warcraft");
+echo $projectedIncome;*/
 
-/* Booking query test
-$booking = new \Entity\BookingEntity(array('seats' => 1, 'users_id' => 1, 'schedules_id' => 1));
-$app['booking_repository']->makeBooking($booking);
- */
+/* Booking query test 
+try {
+    $booking = new \Entity\BookingEntity(array('seats' => 1, 'user_id' => 1, 'schedule_id' => 1));
+    $app['booking_repository']->makeBooking($booking);
+} catch (Exception $ex) {
+    echo $ex->getMessage();
+}*/
+ 
