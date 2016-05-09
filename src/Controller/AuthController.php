0ffe76cf01cacc02bd836fd2c7b33d2ffed90f97
Marius Adam <adamhoreamarius@gmail.com>
2016-05-09 12:59:00 +0300

    [develop] basic login and register

--- src/Controller/AuthController.php
+++ src/Controller/AuthController.php
@@ -25,6 +25,11 @@ class AuthController
         return $app['twig']->render('register.html');
     }
 
+    public function showLogin(Application $app, Request $req)
+    {
+        return $app['twig']->render('login.html');
+    }
+
     public function doRegister(Application $app, Request $req)
     {
         try {
@@ -42,15 +47,49 @@ class AuthController
             if ($userRepo->loadByProperties(['email' => $user->getEmail()])) {
                 throw new \Exception('This email is already associated with another account!');
             }
-            $app['session']->getFlashBag()->add('success', 'Account succesfully created!');
+            $userRepo->save($user);
+            $session->getFlashBag()->add('success', 'Account succesfully created!You can now log in.');
+            $redirect = $app['url_generator']->generate('show_login_page');
+            return $app->redirect($redirect);
+        } catch (\Exception $ex) {
+            $app['session']->getFlashBag()->add('error', $ex->getMessage());
+            return $app['twig']->render('register.html', ['last_email' => $req->get('email')]);
+        }
+    }
+
+    public function doLogin(Application $app, Request $req)
+    {
+        /* @var $session Session */
+        $session = $app['session'];
+        /* @var $userRepo UserRepository */
+        $userRepo = $app['user_repository'];
+        try {
+            $users = $userRepo->loadByProperties(['email' => $req->get('email')]);
+            if (empty($users)) {
+                throw new \Exception('Email not found!');
+            }
+            /* @var $user \Entity\UserEntity */
+            $user = reset($users);
+            if ($user->verifyPassword($req->get('password')) == false) {
+                throw new \Exception('Incorrect password');
+            }
             $session->set('user', $user);
+            $session->getFlashBag()->add('success', 'You are now logeed in!');
             $redirect = $app['url_generator']->generate('show_profile');
             return $app->redirect($redirect);
         } catch (\Exception $ex) {
-            var_dump($_POST);
             $app['session']->getFlashBag()->add('error', $ex->getMessage());
-            return $app['twig']->render('register.html');
+            return $app['twig']->render('login.html');
         }
+    }
+
+    public function doLogout(Application $app, Request $req)
+    {
+        /* @var $session Session */
+        $session = $app['session'];
+        $session->clear();
+        $redirect = $app['url_generator']->generate('homepage');
+        return $app->redirect($redirect);
     }
 
 }
