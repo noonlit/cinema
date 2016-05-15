867e578cbaa893baac32f1d4d0a841e2fd63b113
noonlit <noonlit.github@gmail.com>
2016-05-15 19:54:52 +0300

    [develop] main page can has pagination

--- src/Controller/MainController.php
+++ src/Controller/MainController.php
@@ -9,11 +9,12 @@ class MainController extends AbstractController
 
     public function showMovies() {
         // why do you loop back????
-        $data = $this->session->get('movie_data');
-        if (is_null($data)) {
+        
+        $context = $this->session->get('movie_data');
+        if (is_null($context)) {
             return $this->loadFilteredMovies();
         } else {
-            $pageHtml = $this->render('index', array('data' => $data));
+            $pageHtml = $this->render('index', array('context' => $context));
             $this->session->set('movie_data', null);
             return $pageHtml;
         }
@@ -33,41 +34,63 @@ class MainController extends AbstractController
         if (!is_null($lastForm)) {
             return $lastForm;
         }
-        
+
         // else, no data
         return null;
     }
 
-    public function loadFilteredMovies()
-    {        
-        // repository will fix it if null
-        $page = $this->getQueryParam('page');
-        
-        // set a default value for pagination
-        $perPage = 8;
+    public function loadFilteredMovies($page = 1, $moviesPerPage = 8)
+    {
+        $context = [
+            'movieList' => '',
+            'maxPage' => '',
+            'moviesPerPage' => '',
+            'currentPage' => ''
+        ];
+
+        $movieRepository = $this->getRepository('movie');
+
+        try {
+            $maxMovieNumber = $movieRepository->getRowsCount();
+        } catch (\Exception $ex) {
+            $this->addErrorMessage('Something went wrong!');
+            return $this->render('index', $context);
+        }
+
+        $page = $this->getQueryParam('page') == null ? $page : $this->getQueryParam('page');
+        $moviesPerPage = $moviesPerPage > $maxMovieNumber ? $maxMovieNumber : $moviesPerPage;
 
         // get the conditions for the query, if any
         $conditions = $this->getFormFilterData();
 
         // prepare query data
-        $queryConditions = Helper::prepareQueryData($page, $perPage, $conditions);
+        $queryConditions = Helper::prepareQueryData($page, $moviesPerPage, $conditions);
 
         // get current movies
-        $repository = $this->getRepository('movie');
-        $data = $repository->loadCurrentMovieData($queryConditions);
+        try {
+            $data = $movieRepository->loadCurrentMovieData($queryConditions);
+        } catch (\Exception $ex) {
+            $this->addErrorMessage('Something went wrong!');
+            return $this->render('index', $context);
+        }
+
+        $context = [
+            'movieList' => $data,
+            'maxPage' => $maxMovieNumber / $moviesPerPage,
+            'moviesPerPage' => $moviesPerPage,
+            'currentPage' => $page
+        ];
 
         // keep the results
-        $this->session->set('movie_data', $data);
+        $this->session->set('movie_data', $context);
 
         // go home
         if ($this->request->isMethod('POST')) {
-            return $this->redirectRoute('homepage', array('data' => $data));
+            return $this->redirectRoute('homepage', array('context' => $context));
         } else {
-            return $this->render('index', array('data' => $data));
-        }       
+            return $this->render('index', array('context' => $context));
+        }
     }
-
-
 
     protected function getClassName()
     {
